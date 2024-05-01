461748a2b2c949bac33fe8368985c8e601d42acc
Mansour Tarafdar <tarafdar.mansour@gmail.com>
2024-05-01 14:29:06 +0330

    test(composite): add test for composite sample 1

--- DesignPatternsExample.UnitTest/CompositeUintTest.cs
+++ DesignPatternsExample.UnitTest/CompositeUintTest.cs
@@ -23,5 +23,25 @@ namespace DesignPatternsExample.UnitTest
 
             Assert.Equal(box1.Size(),189);
         }
+
+
+        [Fact]
+        public void WhenIHavePackage_AndIAddProductToPackage_ItShouldAddedToPackageSize()
+        {
+            IComponentPackage box1 = new CompositePackage();
+            box1.Add(new LeafProduct("Pen", 10));
+            box1.Add(new LeafProduct("Cable", 80));
+
+            Assert.Equal(box1.Size(), 90);
+        }
+
+        [Fact]
+        public void WhenIHaveProduct_AndIAddProductToProduct_ItShouldNotAddedToPackageSize()
+        {
+            IComponentPackage product = new LeafProduct("Pen", 10);
+            product.Add(new LeafProduct("Cable", 80));
+
+            Assert.Equal(product.Size(), 10);
+        }
     }
 }
